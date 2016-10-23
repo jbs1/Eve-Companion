31dd8abc0d5463de91a9eaee80cdcf45932a2a9d
Jeremy Schulz <j.schulz@jacobs-univeristy.de>
2016-10-23 07:06:24 +0200

    added oath activites

--- app/src/main/java/com/jbs/evecompanion/MainActivity.java
+++ app/src/main/java/com/jbs/evecompanion/MainActivity.java
@@ -1,7 +1,9 @@
 package com.jbs.evecompanion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**Call login activity on button press**/
+    public void oauth_login(View view) {
+        Intent login=new Intent(this, oauth_login.class);
+        startActivity(login);
+
     }
 }
