b7261b6181f269556d19af5031b521a09f133f24
Jeremy Schulz <j.schulz@jacobs-univeristy.de>
2016-10-23 08:51:23 +0200

    token reqeuest v1

--- app/src/main/java/com/jbs/evecompanion/oauth_callback.java
+++ app/src/main/java/com/jbs/evecompanion/oauth_callback.java
@@ -1,7 +1,19 @@
 package com.jbs.evecompanion;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class oauth_callback extends AppCompatActivity {
 
@@ -9,5 +21,49 @@ public class oauth_callback extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oauth_callback);
+
+        Intent intent = getIntent();
+        Uri data = intent.getData();
+        Log.i("eve_callback_response", data.getQueryParameter("code"));
+        Log.i("eve_callback_response", data.getQueryParameter("state"));
+
+
+        String enc_auth = Base64.encodeToString("9d7dcbb0380f450ea0d2b435b60f4c15:ssEAFgv5PfEs29bluxs9N3milKgC7j6saILCtMPw".getBytes(), Base64.DEFAULT);
+        Log.i("eve_callback_base64",enc_auth);
+
+        String urlParameters  = "grant_type=authorization_code&code="+data.getQueryParameter("code");
+        byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
+        int    postDataLength = postData.length;
+
+
+        URL url = null;
+        try {
+            url = new URL("https://login.eveonline.com/oauth/token");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        HttpURLConnection conn= null;
+        try {
+            conn = (HttpURLConnection) url.openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        conn.setDoOutput( true );
+        try {
+            conn.setRequestMethod( "POST" );
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        }
+        conn.setRequestProperty( "Authorization", enc_auth);
+        conn.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+        conn.setRequestProperty( "charset", "utf-8");
+        conn.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
+        conn.setUseCaches( false );
+        try( DataOutputStream wr = new DataOutputStream( conn.getOutputStream())) {
+            wr.write( postData );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- app/src/main/java/com/jbs/evecompanion/oauth_login.java
+++ app/src/main/java/com/jbs/evecompanion/oauth_login.java
@@ -1,7 +1,10 @@
 package com.jbs.evecompanion;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class oauth_login extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class oauth_login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oauth_login);
+    }
+
+    public void login_to_eve(View view){
+        Uri uri = Uri.parse("https://login.eveonline.com/oauth/authorize/?response_type=code" +
+                "&redirect_uri=x-eve-oauth%3A%2F%2Fcallback" +
+                "&client_id=9d7dcbb0380f450ea0d2b435b60f4c15" +
+                "&scope=characterAccountRead%20characterWalletRead" +
+                "&state=login_eve_first");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 }
