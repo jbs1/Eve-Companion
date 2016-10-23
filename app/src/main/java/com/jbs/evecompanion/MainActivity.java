834f1906937343837d28aa015fcc39a630001488
Jeremy Schulz <j.schulz@jacobs-univeristy.de>
2016-10-23 06:12:38 +0200

    Initial commit

---
+++ app/src/main/java/com/jbs/evecompanion/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jbs.evecompanion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
