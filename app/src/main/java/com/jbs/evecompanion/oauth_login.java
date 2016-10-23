9523b56759e753c3e801a7e5e6695a630d89b7fa
Jeremy Schulz <j.schulz@jacobs-univeristy.de>
2016-10-23 07:01:15 +0200

    added oath activites

---
+++ app/src/main/java/com/jbs/evecompanion/oauth_login.java
@@ -0,0 +1,13 @@
+package com.jbs.evecompanion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class oauth_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oauth_login);
+    }
+}
